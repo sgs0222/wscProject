174c23d218e101c2b1bd5229d149e7134d43162b
CHANMYUNG KIM <cmdrkim@gmail.com>
2015-09-17 16:38:48 +0900

    Maven Profile

--- src/main/java/koreatech/cse/controller/HomeController.java
+++ src/main/java/koreatech/cse/controller/HomeController.java
@@ -1,5 +1,6 @@
 package koreatech.cse.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +9,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeController {
 
+    @Value("${env.text}")
+    private String env;
+
     @RequestMapping
     public String home(Model model) {
         model.addAttribute("textFromController", "World");
+        return "hello";
+    }
+
+    @RequestMapping("/env")
+    public String env(Model model) {
+        model.addAttribute("textFromController", env);
         return "hello";
     }
 }
