710f3b7b976b82032725c19f8af95a55eab526ef
CHANMYUNG KIM <cmdrkim@gmail.com>
2015-09-17 15:30:18 +0900

    mvc 프로젝트 생성

---
+++ src/main/java/koreatech/cse/controller/HomeController.java
@@ -0,0 +1,16 @@
+package koreatech.cse.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @RequestMapping
+    public String home(Model model) {
+        model.addAttribute("textFromController", "World");
+        return "hello";
+    }
+}
