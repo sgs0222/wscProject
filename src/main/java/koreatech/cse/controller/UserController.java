4805e0cc9b17d9b63d9a165ff016c5b050f36367
CHANMYUNG KIM <cmdrkim@gmail.com>
2015-10-05 16:09:32 +0900

    Mybatis, Transactional

--- src/main/java/koreatech/cse/controller/UserController.java
+++ src/main/java/koreatech/cse/controller/UserController.java
@@ -1,21 +1,25 @@
 package koreatech.cse.controller;
 
+import koreatech.cse.domain.Searchable;
 import koreatech.cse.domain.User;
+import koreatech.cse.repository.UserMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import javax.inject.Inject;
 import java.util.List;
 
 @Controller
 
 @RequestMapping("/user")
 public class UserController {
+    @Inject
+    private UserMapper userMapper;
+
 
     @RequestMapping("/signup")
     public String signup(Model model) {
@@ -24,6 +28,7 @@ public class UserController {
         return "signup";
     }
 
+    @Transactional
     @RequestMapping(value="/signup", method= RequestMethod.POST)
     @ResponseBody
     public String signup(@ModelAttribute User user, BindingResult result) {
@@ -34,9 +39,23 @@ public class UserController {
             }
         }
 
+        userMapper.insert(user);
+        //double i = 3 / 0;
+        //System.out.println("i = " + i);
         System.out.println("user = " + user);
         return "success";
+
     }
 
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String list(Model model, @RequestParam(required=false) String name, @RequestParam(required=false) String email, @RequestParam(required=false) String order) {
+        Searchable searchable = new Searchable();
+        searchable.setName(name);
+        searchable.setEmail(email);
+        searchable.setOrderParam(order);
+        model.addAttribute("users", userMapper.findByProvider(searchable));
+        //model.addAttribute("users", userMapper.findByScript(searchable));
+        return "list";
+    }
 
 }
