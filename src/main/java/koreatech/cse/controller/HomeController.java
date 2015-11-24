e54a49040786c43b57bfa83348c3481289311345
guymode <kimhoyun77@naver.com>
2015-11-24 17:16:45 +0900

    no message

--- src/main/java/koreatech/cse/controller/HomeController.java
+++ src/main/java/koreatech/cse/controller/HomeController.java
@@ -37,6 +37,12 @@ public class HomeController {
         return "hello";
     }
 
+    @RequestMapping("/env3")
+    public String env2(Model model) {
+        model.addAttribute("textFromController", env);
+        return "hello";
+    }
+
     @RequestMapping("/requestParamTest")
     public String requestParamTest(@RequestParam(name = "a", required=false, defaultValue = "0") int a,
                                    @RequestParam("b") String b,
