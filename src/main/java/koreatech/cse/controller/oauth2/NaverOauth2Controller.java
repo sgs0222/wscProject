51c872c0d0285bdfca5b0dfb9c62e4d16d0cdcd9
hoyeon <kimhoyun77@naver.com>
2015-12-04 19:15:34 +0900

    hi

---
+++ src/main/java/koreatech/cse/controller/oauth2/NaverOauth2Controller.java
@@ -0,0 +1,7 @@
+package koreatech.cse.controller.oauth2;
+
+/**
+ * Created by user on 2015-12-01.
+ */
+public class NaverOauth2Controller {
+}
---
+++ src/main/java/koreatech/cse/domain/oauth2/naver/NaverProfile.java
@@ -0,0 +1,7 @@
+package koreatech.cse.domain.oauth2.naver;
+
+/**
+ * Created by user on 2015-12-01.
+ */
+public class NaverProfile {
+}
